f8c1d62241ff57b6163e74efac3aeecd4cb9a0ec
Netu0 <osvaldo.beltrame.neto@gmail.com>
2025-11-19 14:13:51 -0300

    feat: add API constants for PostgreSQL and NoSQL services

---
+++ app/const.js
@@ -0,0 +1,3 @@
+export const API_URL_BASE = 'http://192.168.30.66';
+export const API_POSTGRES = `${API_URL_BASE}/api/postgres`;
+export const API_NOSQL = `${API_URL_BASE}/api/nosql`;
--- app/service/user.service.js
+++ app/service/user.service.js
@@ -1,4 +1,14 @@
+import { API_POSTGRES } from "../const.js";
+
 export function get() {
+    fetch(API_POSTGRES + '/auth')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error fetching users:', error);
+        });
 
 }
 
